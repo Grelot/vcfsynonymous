edca812efff3706a4ba3e9f20421c39189e78daa
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-11-17 16:17:21 +0100

    find synonymous variant

--- vcfsynonymous/__main__.py
+++ vcfsynonymous/__main__.py
@@ -73,35 +73,108 @@ genomeGff3File = args.annotation
 vcfFile = args.vcf
 
 
-import numpy
-import biopython
-from BCBio.GFF import GFFExaminer
-from BCBio import GFF
-import pprint
+class CdsSeq:
+    def __init__(self, seqid, start, end, sequence):
+        self.seqid = seqid
+        self.start = start
+        self.end = end
+        self.sequence = sequence
+        self.codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
+
+
+
+def variant_position_within(coordsVar, coordsInterval):
+    """
+    check if coordsVars is within coordsInterval. Return 0
+    """
+    if coordsVar.CHROM == coordsInterval.seqid:
+        if coordsVar.POS >= coordsInterval.start:
+            if coordsVar.POS <= coordsInterval.end:
+                return(1)
+            else:
+                return(0)
+        else:
+            return(0)
+    return(0)
+
+
+def is_synonymous(variant, cds):
+    """
+    check if a variant REF/ALT is synonymous or not from CDS codon in within it is located
+    """
+    posCodon = int((variant.POS - cds.start)/3) 
+    codonVariant = cds.codons[posCodon]
+    posVariantCodon = (variant.POS - cds.start)%3
+    codonRef = list(codonVariant)
+    codonAlt = list(codonVariant)
+    codonRef[posVariantCodon] = str(variant.REF)
+    codonAlt[posVariantCodon] = str(variant.ALT[0])
+    codonRefSeq = Seq(''.join(codonRef))
+    codonAltSeq = Seq(''.join(codonAlt))
+    aaRef = str(codonRefSeq.translate(table=1))
+    aaAlt = str(codonAltSeq.translate(table=1))
+    print(codonVariant, "|||", codonRef, aaRef,"|", codonAlt, aaAlt)
+    if aaRef != aaAlt:
+        return(0)
+    else:
+        return(1)
+
+
 
 
 import gffutils
 import pyfaidx
 from Bio.Seq import Seq
-
+import vcf
+import os
 
 genomeFastaFile = "data_test/genomes/sar_genome_lgt6000.fasta"
-vcf = "data_test/diplodus_sargus_exon.vcf"
+vcfFile = "data_test/diplodus_sargus_exon.vcf"
 genomeGff3File = "data_test/annotation/DSARv1_annotation.gff3"
+dbfnFile = 'currentgff.db'
 
-db = gffutils.create_db(genomeGff3File, dbfn='currentgff.db')
+
+
+## read gff3 and fasta genome
+if os.path.exists(dbfnFile):
+    os.remove(dbfnFile)
+db = gffutils.create_db(genomeGff3File, dbfn=dbfnFile)
 fasta = pyfaidx.Fasta(genomeFastaFile)
 
 ## decompose exon sequence into codon
+cdsSeqList = []
 for cds in db.features_of_type(['CDS','cds'], order_by='start'):
-    cdsSeq = cds.sequence(fasta)    
-    print(cds.start)
-    print(cds.end)
-    print(cds.seqid)
+    cdsSeq = cds.sequence(fasta)
+    print(cds.seqid, cds.start, cds.end)
     cdsSeqStartCodon = cdsSeq.find('ATG')
     if(cdsSeqStartCodon != -1):
-        print(cdsSeqStartCodon)
-        print(cdsSeqStartCodon+cds.start)
-        coding = cdsSeq[cdsSeqStartCodon:]
-        n = 3
-        [coding[i:i+n] for i in range(0, len(coding), n)]
+        cdsSeqID = cds.seqid
+        cdsSeqStart = cdsSeqStartCodon+cds.start + cds.start
+        cdsSeqEnd = cds.end       
+        cdsSeqcoding = cdsSeq[cdsSeqStartCodon:]
+        cdsSeqObj = CdsSeq(cdsSeqID, cdsSeqStart, cdsSeqEnd, cdsSeqcoding)
+        cdsSeqList.append(cdsSeqObj)
+
+
+
+
+## read vcf
+vcf_reader = list(vcf.Reader(open(vcfFile, 'r')))
+
+## check variant is within a cds
+for variant in vcf_reader:
+    #print(variant.CHROM, variant.POS, variant.REF, variant.ALT[0])
+    i=0
+    for cdsSeq in cdsSeqList:        
+        if variant_position_within(variant, cdsSeq):
+            print("cds #", i)
+            print(variant.CHROM,variant.POS, "|", cdsSeq.seqid, cdsSeq.start, cdsSeq.end)
+            is_synonymous(variant, cdsSeq)
+            break
+        i+=1
+
+
+
+
+
+
