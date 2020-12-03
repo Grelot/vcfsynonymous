15aa88af669c29069461587197e69c09ce0d9f6a
Grelot <pierre-edouard.guerin@laposte.net>
2020-12-03 18:16:22 +0100

    fix module

--- vcfsynonymous/__main__.py
+++ vcfsynonymous/__main__.py
@@ -1,5 +1,3 @@
-
-import argparse
 #!/usr/bin/env python3
 #===============================================================================
 #INFORMATION
@@ -22,8 +20,24 @@ import argparse
 #==============================================================================
 #MODULES
 #==============================================================================
+
+## Standard library imports
+import os
 import sys
 import argparse
+
+
+## Third party imports (bioinformatics)
+import gffutils
+import pyfaidx
+from Bio.Seq import Seq
+import vcf
+
+## Local applications import
+from VariantCodon.objets import CdsSeq
+from VariantCodon.dbfasta2CdsSeq import dbfasta2CdsSeq
+from VariantCodon.synonymous import variant_position_within
+from VariantCodon.synonymous import is_synonymous
 
 
 #==============================================================================
@@ -37,10 +51,7 @@ if len(sys.argv) < 2:
    \ \ / /  | |         |  _|    
     \ ' /   \ `.___.'\ _| |_     
      \_/     `.____ .'|_____|    
- ___ _   _ _ __   ___  _ __  _   _ _ __ ___   ___  _   _ ___ 
-/ __| | | | '_ \ / _ \| '_ \| | | | '_ ` _ \ / _ \| | | / __|
-\__ \ |_| | | | | (_) | | | | |_| | | | | | | (_) | |_| \__ \\
-|___/\__, |_| |_|\___/|_| |_|\__, |_| |_| |_|\___/ \__,_|___/
+ ___ _   _ _ __   ___  _ __  _ 59_, |_| |_| |_|\___/ \__,_|___/
      |___/                   |___/                           
                        
 Pierre-Edouard GUERIN, Stephanie MANEL (CNRS, EPHE, Sorbonne University)
@@ -73,61 +84,6 @@ genomeGff3File = args.annotation
 vcfFile = args.vcf
 
 
-class CdsSeq:
-    def __init__(self, seqid, start, end, sequence):
-        self.seqid = seqid
-        self.start = start
-        self.end = end
-        self.sequence = sequence
-        self.codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
-
-
-
-def variant_position_within(coordsVar, coordsInterval):
-    """
-    check if coordsVars is within coordsInterval. Return 0
-    """
-    if coordsVar.CHROM == coordsInterval.seqid:
-        if coordsVar.POS >= coordsInterval.start:
-            if coordsVar.POS <= coordsInterval.end:
-                return(1)
-            else:
-                return(0)
-        else:
-            return(0)
-    return(0)
-
-
-def is_synonymous(variant, cds):
-    """
-    check if a variant REF/ALT is synonymous or not from CDS codon in within it is located
-    """
-    posCodon = int((variant.POS - cds.start)/3) 
-    codonVariant = cds.codons[posCodon]
-    posVariantCodon = (variant.POS - cds.start)%3
-    codonRef = list(codonVariant)
-    codonAlt = list(codonVariant)
-    codonRef[posVariantCodon] = str(variant.REF)
-    codonAlt[posVariantCodon] = str(variant.ALT[0])
-    codonRefSeq = Seq(''.join(codonRef))
-    codonAltSeq = Seq(''.join(codonAlt))
-    aaRef = str(codonRefSeq.translate(table=1))
-    aaAlt = str(codonAltSeq.translate(table=1))
-    print(codonVariant, "|||", codonRef, aaRef,"|", codonAlt, aaAlt)
-    if aaRef != aaAlt:
-        return(0)
-    else:
-        return(1)
-
-
-
-
-import gffutils
-import pyfaidx
-from Bio.Seq import Seq
-import vcf
-import os
-
 #genomeFastaFile = "data_test/genomes/sar_genome_lgt6000.fasta"
 #vcfFile = "data_test/diplodus_sargus_exon.vcf"
 #genomeGff3File = "data_test/annotation/DSARv1_annotation.gff3"
@@ -135,33 +91,19 @@ dbfnFile = 'currentgff.db'
 
 
 
-## read gff3 and fasta genome
+
+## read GFF3 file
 if os.path.exists(dbfnFile):
     os.remove(dbfnFile)
 db = gffutils.create_db(genomeGff3File, dbfn=dbfnFile)
+## read FASTA genome file
 fasta = pyfaidx.Fasta(genomeFastaFile)
-
-## decompose exon sequence into codon
-cdsSeqList = []
-for cds in db.features_of_type(['CDS','cds'], order_by='start'):
-    cdsSeq = cds.sequence(fasta)
-    print(cds.seqid, cds.start, cds.end)
-    cdsSeqStartCodon = cdsSeq.find('ATG')
-    if(cdsSeqStartCodon != -1):
-        cdsSeqID = cds.seqid
-        cdsSeqStart = cdsSeqStartCodon+cds.start + cds.start
-        cdsSeqEnd = cds.end       
-        cdsSeqcoding = cdsSeq[cdsSeqStartCodon:]
-        cdsSeqObj = CdsSeq(cdsSeqID, cdsSeqStart, cdsSeqEnd, cdsSeqcoding)
-        cdsSeqList.append(cdsSeqObj)
-
-
-
-
-## read vcf
+## From the genome(GFF3, FASTA),
+## extract a list of CDS (coding sequences) objects
+cdsSeqList = dbfasta2CdsSeq(db, fasta)
+## read VCF file
 vcf_reader = list(vcf.Reader(open(vcfFile, 'r')))
-
-## check variant is within a cds
+## check wether variant is within a CDS
 for variant in vcf_reader:
     #print(variant.CHROM, variant.POS, variant.REF, variant.ALT[0])
     i=0
