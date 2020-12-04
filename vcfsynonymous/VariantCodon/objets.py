4346c7a1c5622b0bbd7518a26b419efdba18f473
Grelot <pierre-edouard.guerin@laposte.net>
2020-12-04 17:32:07 +0100

    add eq method to cdsseq objet

--- vcfsynonymous/VariantCodon/objets.py
+++ vcfsynonymous/VariantCodon/objets.py
@@ -5,3 +5,8 @@ class CdsSeq:
         self.end = end
         self.sequence = sequence
         self.codons = [sequence[i:i+3] for i in range(0, len(sequence), 3)]
+    def __eq__(self, other): 
+        if not isinstance(other, CdsSeq):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return self.seqid == other.seqid and self.start == other.start and self.end == other.end and self.sequence == other.sequence and self.codons == other.codons
