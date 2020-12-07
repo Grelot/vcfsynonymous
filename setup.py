73d2459a24853a50061ed6cc710b3124cc2f17d7
Grelot <pierre-edouard.guerin@laposte.net>
2020-12-07 12:02:09 +0100

    update setup pipy

--- setup.py
+++ setup.py
@@ -5,36 +5,22 @@ with open("doc/pypi_readme.md", "r") as fh:
 
 setuptools.setup(
 
-     name='vcfsynonymous',  
-
-     version='0.1.0',
-
-     scripts=['src/demort.py'] ,
-
-     author="Pierre-Edouard GUERIN",
-
-     author_email="pierre-edouard.guerin@cefe.cnrs.fr",
-
-     description="Detect synonymous genetic variants in VCF",
-
-     long_description_content_type="text/markdown",
-
-     long_description=long_description,
-     
-
-     url="https://github.com/Grelot/vcfsynonymous",
-
-     packages=setuptools.find_packages(),
-
+    name='vcfsynonymous',
+    version='0.1.1',
+    scripts=['vcfsynonymous/__main.py__'] ,
+    author="Pierre-Edouard GUERIN",
+    author_email="pierre-edouard.guerin@cefe.cnrs.fr",
+    description="Detect synonymous genetic variants in VCF",
+    licence="MIT",
+    url="https://github.com/Grelot/vcfsynonymous",
+    packages=setuptools.find_packages(),
     install_requires=['argparse', 'numpy', 'biopython', 'bcbio-gff', 'pyfaidx', 'gffutils', 'PyVCF'],
 
-     classifiers=[
-
+    classifiers=[
          "Programming Language :: Python :: 3",
-
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
          "License :: OSI Approved :: MIT License",
-
-         "Operating System :: OS Independent",
-
-     ],
+         "Operating System :: OS Independent"
+     ]
 )
